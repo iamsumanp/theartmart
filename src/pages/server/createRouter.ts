ac7868430ae6e896f1ad35c3fa7574fa6199baf0
Suman Pokharel <sumanpok9@gmail.com>
2022-08-04 22:46:54 +0545

    delete trpc, complexity level high

--- src/pages/server/createRouter.ts
+++
@@ -1,6 +0,0 @@
-import * as trpc from "@trpc/server";
-import type { MyContextType } from "./context";
-
-export function createRouter() {
-  return trpc.router<MyContextType>();
-}
--- src/pages/server/routers/app.ts
+++
@@ -1,8 +0,0 @@
-//this is the root route
-
-import { createRouter } from "../createRouter";
-import { nameRouter } from "./names";
-
-export const appRouter = createRouter().merge("names.", nameRouter);
-
-export type AppRouter = typeof appRouter;
